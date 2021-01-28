96aaeee790806a0e25e5ebab363f857e8dbe6178
Rahima Salmanova <64548507+rahimasalman@users.noreply.github.com>
2021-01-28 23:38:48 +0400

    Add files via upload

---
+++ module4_solution/SpeakHello.js
@@ -0,0 +1,16 @@
+(function(window){
+
+
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
